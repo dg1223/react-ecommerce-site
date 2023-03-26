f363d66d52ae9dc7a1696b51dfdbaff0c8454c84
dg1223 <alavi1223@hotmail.com>
2023-03-26 18:55:53 -0400

    created header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import "./Header.css";
+import logo from "../../images/Logo.svg";
 
 const Header = () => {
   return (
-    <div>
-      <h2>This is a header</h2>
-    </div>
+    <nav className="header">
+      <img src={logo} alt="" />
+      <div>
+        <a href="/Order">Order</a>
+        <a href="/Order Review">Order Review</a>
+        <a href="/Manage Inventory">Manage Inventory</a>
+        <a href="/Login">Login</a>
+      </div>
+    </nav>
   );
 };
 
