d71a5b759ac3654107b6d38f0e1d3df02c67c77a
dg1223 <alavi1223@hotmail.com>
2023-03-29 22:00:26 -0400

    add products to cart, buggy

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,13 +7,16 @@ const Cart = ({ cart }) => {
   //   const cart = props.cart;
   // option 2
   //   const { cart } = props;
-  console.log(cart);
+  // console.log(cart);
 
   let totalPrice = 0;
   let totalShipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    totalPrice = totalPrice + product.price;
+    product.quantity = product.quantity || 1;
+    totalPrice = totalPrice + product.price * product.quantity;
     totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
   }
 
   const tax = totalPrice * 0.07;
@@ -23,7 +26,7 @@ const Cart = ({ cart }) => {
   return (
     <div className="cart">
       <h4>Order Summary</h4>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${totalPrice}</p>
       <p>Total Shipping Charge: ${totalShipping}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -13,31 +13,56 @@ const Shop = () => {
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
+      // get an array of objects (products)
       .then((data) => setProducts(data));
   }, []);
 
   useEffect(() => {
     const storedCart = getShoppingCart();
     const savedCart = [];
-    // step 1: Get ID
+    // step 1: Get ID of the added / stored product
+    // for...in: on object; for...of: on array (of objects)
     for (const id in storedCart) {
-      // step 2: get the product by using ID
-      const addedProducts = products.find((product) => product.id == id);
-      if (addedProducts) {
-        // step 3: get quantity of the product
+      // step 2: Get product from products state by using ID
+      // an object
+      const addedProduct = products.find((product) => product.id === id);
+      // if product is found in local storage (shopping cart)
+      if (addedProduct) {
+        // step 3: Add quantity
+        // get quantity from local storage
         const quantity = storedCart[id];
-        addedProducts.quantity = quantity;
+        // update quantity of the product object stored as addedProduct
+        addedProduct.quantity = quantity;
         // step 4: add the added product to the saved cart
-        savedCart.push(addedProducts);
+        savedCart.push(addedProduct);
       }
-      console.log(addedProducts);
+      console.log("added Product: ", addedProduct);
     }
     // step 5: set the cart
     setCart(savedCart);
+    /* Since hooks are asynchronous, we get the default value of
+    at first, which is an empty array. The product will get
+    loaded a little later. As a result, if we use empty array
+    [] as useEffect's dependency, we won't see the product. So,
+    we need to use 'products' as the dependency so see the
+    actual products after their values are loaded. */
   }, [products]);
 
   const handleAddToCart = (product) => {
-    const newCart = [...cart, product];
+    let newCart = [];
+    // const newCart = [...cart, product];
+    // if product doesn't exist in the cart, then set quantity = 1
+    // if product exists, update quantity by 1
+    const exists = cart.find((pd) => pd.id === product.id);
+    if (!exists) {
+      product.quantity = 1;
+      newCart = [...cart, product];
+    } else {
+      exists.quantity = exists.quantity + 1;
+      const remaining = cart.filter((pd) => pd.id !== product.id);
+      newCart = [...cart, exists];
+    }
+
     setCart(newCart);
     addToDb(product.id);
   };
