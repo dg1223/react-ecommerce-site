028ffadcaff243d47372aefd831308795578e80f
dg1223 <alavi1223@hotmail.com>
2023-04-04 16:20:04 -0400

    share data between pages

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,3 +1,5 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
 /**
  * The cartProductsLoader function loads the products from the product.json file and returns them as an array of objects
  *
@@ -7,9 +9,31 @@
  *
  */
 const cartProductsLoader = async () => {
-  const loadedProducts = await fetch("product.json");
+  const loadedProducts = await fetch("products.json");
   const products = await loadedProducts.json();
-  console.log(products);
+  const savedCart = [];
+
+  // if cart data is in database, you must use async await
+  const storedCart = getShoppingCart();
+  // console.log(storedCart);
+  for (const id in storedCart) {
+    const addedProduct = products.find((product) => product.id === id);
+    if (addedProduct) {
+      const quantity = storedCart[id];
+      addedProduct.quantity = quantity;
+      // stores the entire product object in an array
+      savedCart.push(addedProduct);
+    }
+  }
+  // if you need to return more than 1 item
+  /* // option 1: return inside an array
+  return [products, savedCart] */
+
+  /*   // option 2: return as an object
+  return {products, cart: savedCart} */
+
+  // console.log(savedCart);
+  return savedCart;
 };
 
 export default cartProductsLoader;
