922845555fe5489277449c53bdd3c0b9e573f837
dg1223 <alavi1223@hotmail.com>
2023-04-04 16:50:29 -0400

    cart items added to orders page

--- src/components/ReviewItem/ReviewItem.jsx
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import "./ReviewItem.css";
 
-const ReviewItem = () => {
+const ReviewItem = ({ product }) => {
+  const { id, img, price, name, quantity } = product;
   return (
-    <div>
-      <h2>Review item</h2>
+    <div className="review-item">
+      <img src={img} alt="" />
     </div>
   );
 };
