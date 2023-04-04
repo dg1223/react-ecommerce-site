364c5bcfdb120e9f550580e56ed4a097a7a4d82d
dg1223 <alavi1223@hotmail.com>
2023-04-04 17:54:38 -0400

    shopping cart functionality complete

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Cart from "../Cart/Cart";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useSearchParams } from "react-router-dom";
 import ReviewItem from "../ReviewItem/ReviewItem";
 import "./Orders.css";
+import { removeFromDb } from "../../utilities/fakedb";
 
 const Orders = () => {
-  const cart = useLoaderData();
-  // console.log(cart);
+  const savedCart = useLoaderData();
+  const [cart, setCart] = useState(savedCart);
+
+  const handleRemoveFromCart = (id) => {
+    // get all other items from the cart
+    const remaining = cart.filter((product) => product.id != id);
+    setCart(remaining);
+    removeFromDb(id);
+  };
+  // console.log(savedCart);
   return (
     <div className="shop-container">
       <div className="review-container">
         {cart.map((product) => (
-          <ReviewItem key={product.id} product={product}></ReviewItem>
+          <ReviewItem
+            key={product.id}
+            product={product}
+            handleRemoveFromCart={handleRemoveFromCart}
+          ></ReviewItem>
         ))}
       </div>
       <div className="cart-container">
