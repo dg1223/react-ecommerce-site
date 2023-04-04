0025464f592c45581ed73a12b33e705adedc4a34
dg1223 <alavi1223@hotmail.com>
2023-04-04 14:27:08 -0400

    set up router for navigation

---
+++ src/components/Inventory/Invetory.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Invetory = () => {
+  return (
+    <div>
+      <h3>Invetory page</h3>
+    </div>
+  );
+};
+
+export default Invetory;
--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,43 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Layout/Home";
 import Shop from "./components/Shop/Shop";
+import Orders from "./components/Orders/Orders";
+import Invetory from "./components/Inventory/Invetory";
+import Login from "./components/Login/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Shop></Shop>,
+    element: <Home></Home>,
+    children: [
+      {
+        path: "/",
+        element: <Shop></Shop>,
+      },
+      {
+        path: "shop",
+        element: <Shop></Shop>,
+      },
+      {
+        path: "orders",
+        element: <Orders></Orders>,
+      },
+      {
+        path: "inventory",
+        element: <Invetory></Invetory>,
+      },
+      {
+        path: "login",
+        element: <Login></Login>,
+      },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
